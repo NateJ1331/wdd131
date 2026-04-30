6052fba6f1afc0f2071b562208e92cf1fa38b63a
NateJ1331 <nathan.apple131@gmail.com>
2026-04-29 19:47:31 -0600

    folled video instruction

---
+++ ponder/dom_practice/dom.js
@@ -0,0 +1,25 @@
+const title = document.querySelector('h1');
+
+title.textContent = 'Web page Componenets';
+
+let topics = document.querySelector('#topics')
+
+//topics.style.color = 'red';
+
+document.getElementById('topics').style.color = 'purple';
+
+let list = document.querySelector('.list');
+
+list.style.border = '3px solid black';
+
+let para = document.querySelector('p');
+
+//para.style.backgroundColor = 'lightblue';
+
+para.classList.add('backround');
+
+//document.querySelector('body').classList.add('backround');
+
+const image = document.querySelector('img');
+image.setAttribute('src','images/newimg.png')
+
