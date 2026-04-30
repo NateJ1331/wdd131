b9cbe195cb3651b7e34bb8a48d3f77f0ca5c3651
NateJ1331 <nathan.apple131@gmail.com>
2026-04-29 19:51:37 -0600

    finished up javascript

--- ponder/dom_practice/dom.js
+++ ponder/dom_practice/dom.js
@@ -23,3 +23,12 @@ para.classList.add('backround');
 const image = document.querySelector('img');
 image.setAttribute('src','images/newimg.png')
 
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+    document.getElementById(codeValue).style.color = 'red';
+})
+                
+                
