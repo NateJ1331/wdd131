2e62825bed49c3c0f140c901aaec9ad97a1e7343
NateJ1331 <nathan.apple131@gmail.com>
2026-04-29 22:17:26 -0600

    Finished mission javascrip

---
+++ mission/main.js
@@ -0,0 +1,24 @@
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+pageContent = document.querySelector('#content');
+underline = document.querySelector('h2');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        document.body.style.backgroundColor = ("black"); 
+        document.body.style.color = ("white");
+        pageContent.style.borderColor = ("white");
+        underline.style.borderColor = ("white");
+        logo.setAttribute('src','assets/byui-logo-white.png')
+        
+    } else {
+        document.body.style.backgroundColor = ("white"); 
+        document.body.style.color = ("black");
+        pageContent.style.borderColor = ("black");
+        underline.style.borderColor = ("black");
+        logo.setAttribute('src','assets/byui-logo-blue.png')
+    }
+}     
